e141b0a019cf3da09c94d72168efaa9acb387023
Edgar Teixeira <edgar.tx@outlook.com>
2018-10-10 11:40:05 -0300

    Language Classifier Baseline
    Finished the baseline of the Language Classifer. This version implements
    a TF-IDF + RandomForestClassifier pipeline. It achives an accuracy of
    ~96% in this dataset.

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -1,6 +1,8 @@
+import random
 import re
 import tarfile
 import unicodedata
+from enum import Enum
 from typing import List
 
 
@@ -13,9 +15,11 @@ def data_archive(archive: str, encoding: str) -> str:
             if fhandler is not None:
                 content = fhandler.read()\
                                   .decode(encoding)\
-                                  .replace("\\x93", '"')\
-                                  .replace("\\x94", '"')\
-                                  .replace("\\x97", '-')
+                                  .replace("\x92", '')\
+                                  .replace("\x93", '')\
+                                  .replace("\x94", '')\
+                                  .replace("\x96", '')\
+                                  .replace("\x97", '')
                 corpus.append(content)
                 fhandler.close()
 
@@ -30,7 +34,7 @@ def remove_accents(text: str) -> str:
 
 def remove_punctuations(text: str) -> str:
     # TODO: Should ignore numbers? Like 3.0 or 3,0
-    return re.sub(r"[!\"#$%&()*+,-./:;<=>?@[\\\]^_`{|}~]+", " ", text)
+    return re.sub(r"[!\"#$%&()*+,-./:;<=>?@[\\\]^_`{|}~“”—‘’]+", " ", text)
 
 
 def remove_case(text: str) -> str:
@@ -45,3 +49,56 @@ def build_ngrams(text: str, size: int) -> List[str]:
     if size <= 0:
         raise ValueError("size must be greater than 0")
     return [text[idx:idx + size] for idx in range(len(text) - size + 1)]
+
+
+def generate_samples(text: str, sample_size: int, seed: int) -> List[str]:
+    if sample_size < 1:
+        raise ValueError("sample_size must be greater than 1")
+
+    samples = [""] * sample_size
+    prng_state = random.getstate()
+    random.seed(seed)
+
+    spaces = list(re.finditer(r"\s+", text))
+    num_spaces = len(spaces)
+
+    for i in range(sample_size):
+        begin = random.randint(0, num_spaces - 1)
+        end = begin + random.randint(1, 15)
+        if end >= num_spaces:
+            end = num_spaces - 1
+
+        samples[i] = text[spaces[begin].end():spaces[end].start()]
+
+    random.setstate(prng_state)
+
+    return samples
+
+
+class OOVPolicy(Enum):
+    ignore = 0
+    error = 1
+    encode = 2
+
+
+class OneHotEncoder:
+    def __init__(self, oov_policy: OOVPolicy = OOVPolicy.ignore) -> None:
+        if oov_policy != OOVPolicy.ignore:
+            raise NotImplementedError(
+                "The current version only supports the ignore policy")
+
+        self.oov_policy = oov_policy
+
+    def fit(self, X: List[str]) -> None:
+        alphabet = set(''.join(X))
+        self.mapping_size_ = len(alphabet)
+        self.mapping_ = dict(zip(alphabet, range(self.mapping_size_)))
+
+    def transform(self, X: str) -> List[int]:
+        features = [0] * self.mapping_size_
+
+        for char in X:
+            if char in self.mapping_:
+                features[self.mapping_[char]] += 1
+
+        return features
