4e286492098e94a6d7a5627fe88c66dd5cea7744
Edgar Teixeira <edgar.tx@outlook.com>
2018-11-01 15:55:50 -0300

    Language Classifier v2
    New version of the Language Classifier. The model now is a pipeline of 3
    steps: CountVectorizer, TfidfTransformer and LogisticRegression. The
    vectorizer use characters and n-grams as tokens and the accents are
    kept. The new version achieves 99.62% accuracy.

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -1,8 +1,6 @@
 import random
 import re
 import tarfile
-import unicodedata
-from enum import Enum
 from typing import List
 
 
@@ -26,19 +24,9 @@ def data_archive(archive: str, encoding: str) -> str:
     return '\n\n'.join(corpus)
 
 
-def remove_accents(text: str) -> str:
-    return "".join(
-        c for c in unicodedata.normalize("NFD", text)
-        if unicodedata.category(c) != 'Mn')
-
-
 def remove_punctuations(text: str) -> str:
     # TODO: Should ignore numbers? Like 3.0 or 3,0
     return re.sub(r"[!\"#$%&()*+,-./:;<=>?@[\\\]^_`{|}~“”—‘’']+", " ", text)
-
-
-def remove_case(text: str) -> str:
-    return text.lower()
 
 
 def flatten_text(text: str) -> str:
@@ -73,32 +61,3 @@ def generate_samples(text: str, sample_size: int, seed: int) -> List[str]:
     random.setstate(prng_state)
 
     return samples
-
-
-class OOVPolicy(Enum):
-    ignore = 0
-    error = 1
-    encode = 2
-
-
-class IndexEncoder:
-    def __init__(self, oov_policy: OOVPolicy = OOVPolicy.ignore) -> None:
-        if oov_policy != OOVPolicy.ignore:
-            raise NotImplementedError(
-                "The current version only supports the ignore policy")
-
-        self.oov_policy = oov_policy
-
-    def fit(self, X: List[str]) -> None:
-        alphabet = set(''.join(X))
-        self.mapping_size_ = len(alphabet)
-        self.mapping_ = dict(zip(alphabet, range(self.mapping_size_)))
-
-    def transform(self, X: str) -> List[int]:
-        features = [0] * self.mapping_size_
-
-        for char in X:
-            if char in self.mapping_:
-                features[self.mapping_[char]] += 1
-
-        return features
--- scripts/train.py
+++ scripts/train.py
@@ -1,6 +1,6 @@
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
@@ -15,13 +15,10 @@ print('OK')
 
 # Clean the data
 print("Cleaning data..   ", end='', flush=True)
-portuguese_corpus = pipe.remove_case(
-    pipe.remove_accents(
-        pipe.flatten_text(pipe.remove_punctuations(portuguese_corpus))))
+portuguese_corpus = pipe.flatten_text(
+    pipe.remove_punctuations(portuguese_corpus))
 
-english_corpus = pipe.remove_case(
-    pipe.remove_accents(
-        pipe.flatten_text(pipe.remove_punctuations(english_corpus))))
+english_corpus = pipe.flatten_text(pipe.remove_punctuations(english_corpus))
 print('OK')
 
 # Build dataset
@@ -31,28 +28,25 @@ x_dev = pipe.generate_samples(portuguese_corpus, data_size, 2018) +\
         pipe.generate_samples(english_corpus, data_size, 2018)
 y_dev = [0] * data_size + [1] * data_size
 
-encoder = pipe.OneHotEncoder(pipe.OOVPolicy.ignore)
-encoder.fit(x_dev)
-x_feature = list(map(encoder.transform, x_dev))
-
-x_feature = np.asarray(x_feature)
-y_feature = np.asarray(y_dev)
-
-x_train, x_val, y_train, y_val, raw_train, raw_val = train_test_split(
-    x_feature,
-    y_feature,
-    x_dev,
-    test_size=0.33,
-    shuffle=True,
-    random_state=2018)
+x_train, x_val, y_train, y_val = train_test_split(
+    x_dev, y_dev, test_size=0.33, shuffle=True, random_state=2018)
 print('OK')
 
 # Train model
 print("Training model..   ", end='', flush=True)
 model = make_pipeline(
-    TfidfTransformer(),
-    RandomForestClassifier(
-        n_estimators=512, class_weight='balanced', n_jobs=-1))
+    CountVectorizer(
+        input='content',
+        strip_accents=None,
+        lowercase=True,
+        ngram_range=(1, 4),
+        analyzer='char'), TfidfTransformer(),
+    LogisticRegression(
+        C=100,
+        class_weight='balanced',
+        n_jobs=-1,
+        solver='lbfgs',
+        max_iter=2000))
 model.fit(x_train, y_train)
 y_pred = model.predict(x_val)
 print("OK")
@@ -72,5 +66,5 @@ errors_sample = np.random.choice(errors, size=10)
 print("Error Analysis")
 print('-' * 20)
 for idx in errors_sample:
-    print(raw_val[idx])
+    print(x_val[idx])
     print()
